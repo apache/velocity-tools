e86b08bd56c9557672150fdbc8acd050bab89441
Daniel Rall <dlr@apache.org>
2002-04-02 18:51:46 +0000

    Allows for transparent content negotiation in a manner mimicking Apache httpd's MultiViews.  Example usage from a template would be something like the following:
      #parse ($multiviews.findLocalizedResource("header.vm"))
      #include ($multiviews.findLocalizedResource("my_page.html"))
      #parse ($multiviews.findLocalizedResource("footer.vm"))
    
    
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity-tools/trunk@71572 13f79535-47bb-0310-9956-ffa450edef68

---
+++ view/src/java/org/apache/velocity/tools/view/i18n/MultiViewsTool.java
@@ -0,0 +1,188 @@
+/*
+ * The Apache Software License, Version 1.1
+ *
+ * Copyright (c) 2001 The Apache Software Foundation.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution, if
+ *    any, must include the following acknowlegement:
+ *       "This product includes software developed by the
+ *        Apache Software Foundation (http://www.apache.org/)."
+ *    Alternately, this acknowlegement may appear in the software itself,
+ *    if and wherever such third-party acknowlegements normally appear.
+ *
+ * 4. The names "The Jakarta Project", "Velocity", and "Apache Software
+ *    Foundation" must not be used to endorse or promote products derived
+ *    from this software without prior written permission. For written
+ *    permission, please contact apache@apache.org.
+ *
+ * 5. Products derived from this software may not be called "Apache"
+ *    nor may "Apache" appear in their names without prior written
+ *    permission of the Apache Group.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ */
+
+
+package org.apache.velocity.tools.view.i18n;
+
+import java.util.Locale;
+import javax.servlet.ServletContext;
+
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.context.Context;
+
+import org.apache.velocity.tools.view.context.ViewContext;
+import org.apache.velocity.tools.view.tools.ContextTool;
+
+/**
+ * <p>Allows for transparent content negotiation in a manner mimicking
+ * Apache httpd's <a
+ * href="http://httpd.apache.org/docs-2.0/content-negotiation.html">MultiViews</a></p>.
+ *
+ * <p>Reads the default language out of the ViewContext as
+ * <code>org.apache.velocity.tools.view.i18n.defaultLanguage</code>.
+ * See {@link #findLocalizedResource(String, String)} and {@link
+ * #findLocalizedResource(String, Locale)} for usage.</p>
+ *
+ * @author <a href="mailto:dlr@finemaltcoding.com">Daniel Rall</a>
+ */
+public class MultiViewsTool
+    implements ContextTool
+{
+    protected static final String DEFAULT_LANGUAGE_KEY =
+        "org.apache.velocity.tools.view.i18n.defaultLanguage";
+
+    protected String defaultLanguage;
+
+    /**
+     * Creates a new uninitialized instance.  Call {@link
+     * #init(ViewContext)} to initialize it.
+     */
+    public MultiViewsTool()
+    {
+    }
+
+    /**
+     * Extracts the default language from the specified
+     * <code>ViewContext</code>, looking first at the servlet context,
+     * then the velocity context, then lastly at the JVM default.
+     *
+     * @param context The context to use.
+     * @return A reference to this object (i.e. ourself).
+     * @see org.apache.velocity.view.tools.ContextTool
+     */
+    public Object init(ViewContext context)
+    {
+        ServletContext sc = context.getServletContext();
+        defaultLanguage = (String) sc.getAttribute(DEFAULT_LANGUAGE_KEY);
+        if (defaultLanguage == null || defaultLanguage.trim().equals(""))
+        {
+            Context vc = context.getVelocityContext();
+            defaultLanguage = (String) vc.get(DEFAULT_LANGUAGE_KEY);
+            if (defaultLanguage == null || defaultLanguage.trim().equals(""))
+            {
+                // Use JVM default.
+                defaultLanguage = Locale.getDefault().getLanguage();
+            }
+        }
+        return this;
+    }
+
+    /**
+     * At the end of processing this method will be called to 
+     * return the object generated by init(), in case it needs
+     * to be recycled or otherwise cleaned up.
+     */
+    public void destroy(Object obj)
+    {
+        defaultLanguage = null;
+    }
+
+    /**
+     * Calls {@link #findLocalizedResource(String, String)} using the
+     * language extracted from <code>locale</code>.
+     *
+     * @see #findLocalizedResource(String, String)
+     */
+    public String findLocalizedResource(String name, Locale locale)
+    {
+        return findLocalizedResource(name, locale.getLanguage());
+    }
+
+    /**
+     * <p>Finds the a localized version of the requested Velocity
+     * resource (such as a file or template) which is most appropriate
+     * for the locale of the current request.  Use in conjuction with
+     * Apache httpd's <code>MultiViews</code>, or by itself.</p>
+     *
+     * <p>Usage from a template would be something like the following:
+     * <blockquote><code><pre>
+     * #parse ($multiviews.findLocalizedResource("header.vm"))
+     * #include ($multiviews.findLocalizedResource("my_page.html"))
+     * #parse ($multiviews.findLocalizedResource("footer.vm"))
+     * </pre></code></blockquote>
+     *
+     * You might also wrap this method using another pull/context tool
+     * which does internationalization/localization/content negation
+     * for a single point of access.</p>
+     *
+     * @param name The unlocalized name of the file to find.
+     * @param language The language to find localized context for.
+     * @return The localized file name, or <code>name</code> if it is
+     * not localizable.
+     */
+    public String findLocalizedResource(String name, String language)
+    {
+        String localizedName = name + '.' + language;
+        // templateExists() checks for static content as well
+        if (!Velocity.templateExists(localizedName))
+        {
+            // Fall back to the default lanaguage.
+            String defaultLangSuffix = '.' + defaultLanguage;
+            if (localizedName.endsWith(defaultLangSuffix))
+            {
+                // Assume no localized version of the resource.
+                localizedName = name;
+            }
+            else
+            {
+                localizedName = name + defaultLangSuffix;
+                if (!Velocity.templateExists(localizedName))
+                {
+                    localizedName = name;
+                }
+            }
+        }
+        return localizedName;
+    }
+}
