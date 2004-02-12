68f7ceec9e266c89ce2dd31d0de9c21f75e8a6a5
Nathan Bubna <nbubna@apache.org>
2004-02-12 18:10:16 +0000

    demo just a few more things in simple
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity-tools/trunk@71966 13f79535-47bb-0310-9956-ffa450edef68

--- examples/simple/WEB-INF/src/ToyTool.java
+++ examples/simple/WEB-INF/src/ToyTool.java
@@ -1,7 +1,20 @@
 public class ToyTool
 {
+    private String message = "Hello from ToyTool!";
+
 	public String getMessage()
 	{
-        return "Hello from ToyTool!";
+        return message;
 	}
+    
+    public void setMessage(String m)
+    { 
+        message = m;
+    }
+
+    /** To test exception handling in templates. */
+    public boolean whine() {
+        throw new IllegalArgumentException();
+    }
+
 }
