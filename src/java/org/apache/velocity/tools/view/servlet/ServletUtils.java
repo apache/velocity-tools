59f74c7bab56fc48871771b37d0d175f0b127de6
Nathan Bubna <nbubna@apache.org>
2006-11-14 20:32:06 +0000

    extract a getPath(request) method to a utility class for easy reuse
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/velocity/tools/trunk@474951 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/velocity/tools/view/servlet/ServletUtils.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.velocity.tools.view.servlet;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * <p>A set of utility methods for the servlet environment.</p>
+ *
+ * @version $Id: ServletUtils.java 471244 2006-11-04 18:34:38Z henning $
+ */
+public class ServletUtils
+{
+
+    /**
+     * Retrieves the path for the specified request regardless of
+     * whether this is a direct request or an include by the
+     * RequestDispatcher.
+     */
+    public static String getPath(HttpServletRequest request)
+    {
+        // If we get here from RequestDispatcher.include(), getServletPath()
+        // will return the original (wrong) URI requested.  The following special
+        // attribute holds the correct path.  See section 8.3 of the Servlet
+        // 2.3 specification.
+        String path = (String)request.getAttribute("javax.servlet.include.servlet_path");
+        // also take into account the PathInfo stated on SRV.4.4 Request Path Elements
+        String info = (String)request.getAttribute("javax.servlet.include.path_info");
+        if (path == null)
+        {
+            path = request.getServletPath();
+            info = request.getPathInfo();
+        }
+        if (info != null)
+        {
+            path += info;
+        }
+        return path;
+    }
+    
+}
