34b903980dd8bd09535c94e47da9b113a1351f8c
Nathan Bubna <nbubna@apache.org>
2007-03-22 18:18:22 +0000

    add back moved classes as deprecated subclasses for backwards compatibility
    git-svn-id: https://svn.apache.org/repos/asf/velocity/tools/branches/2.x@521392 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/velocity/tools/view/servlet/ServletLogger.java
@@ -0,0 +1,46 @@
+package org.apache.velocity.tools.view.servlet;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.velocity.runtime.log.LogSystem;
+import org.apache.velocity.tools.view.ServletLogChute;
+
+/**
+ * <p>This is basically an empty subclass of {@link ServletLogChute} that exists
+ *    merely for backwards compatibility with VelocityTools 1.x. Please
+ *    use {@link ServletLogChute} directly, as this will likely be removed
+ *    in VelocityTools 2.1, if not earlier.
+ * </p>
+ *
+ * @author <a href="mailto:geirm@apache.org">Geir Magnusson Jr.</a>
+ * @deprecated Use {@link ServletLogChute} instead
+ * @version $Revision$ $Date: 2007-02-26 11:24:39 -0800 (Mon, 26 Feb 2007) $
+ */
+public class ServletLogger extends ServletLogChute implements LogSystem
+{
+    /**
+     * Send a log message from Velocity.
+     */
+    public void logVelocityMessage(int level, String message)
+    {
+        log(level, message);
+    }
+
+}
