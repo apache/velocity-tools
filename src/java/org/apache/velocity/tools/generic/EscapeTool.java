467e370d8d389e8d7dd9e6130ac0656f035bbfaa
Claude Brisson <cbrisson@apache.org>
2007-06-29 14:15:57 +0000

    little javadoc fix backported to tools 1.x
    git-svn-id: https://svn.apache.org/repos/asf/velocity/tools/trunk@551907 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/velocity/tools/generic/EscapeTool.java
+++ src/java/org/apache/velocity/tools/generic/EscapeTool.java
@@ -46,8 +46,8 @@ import org.apache.commons.lang.StringEscapeUtils;
  *  $sql                         -> McHale's Navy
  *  $esc.sql($sql)               -> McHale''s Navy
  *
- *  $http                        -> hello here & there
- *  $esc.http                    -> hello+here+%26+there
+ *  $url                         -> hello here & there
+ *  $esc.url                     -> hello+here+%26+there
  *
  *  $esc.dollar                  -> $
  *  $esc.d                       -> $
