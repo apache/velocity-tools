9f79946ef7b510ba2a9ecf525266d8e95cc8a96a
Antonio Petrelli <apetrelli@apache.org>
2010-12-17 15:43:05 +0000

    VELTOOLS-132 Added documentation for the plugin. Refactored velocity-tools-view-jsp a bit.
    git-svn-id: https://svn.apache.org/repos/asf/velocity/tools/trunk@1050428 13f79535-47bb-0310-9956-ffa450edef68

--- velocity-tools-view-jsp/src/main/java/org/apache/velocity/tools/view/jsp/jspimpl/JspUtils.java
+++ velocity-tools-view-jsp/src/main/java/org/apache/velocity/tools/view/jsp/jspimpl/JspUtils.java
@@ -1,4 +1,23 @@
-package org.apache.velocity.tools.view.jsp.taglib.jspimpl;
+package org.apache.velocity.tools.view.jsp.jspimpl;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 import java.io.IOException;
 
@@ -16,18 +35,42 @@ import org.apache.velocity.context.InternalContextAdapter;
 import org.apache.velocity.runtime.parser.node.ASTBlock;
 import org.apache.velocity.runtime.parser.node.Node;
 
+/**
+ * Some utilities to work with JSP.
+ */
 public class JspUtils
 {
+    /**
+     * The attribute name under which the latest encountered tag is put.
+     */
     private final static String LATEST_TAG_ATTRIBUTE_NAME = "org.apache.velocity.tools.view.jsp.taglib.jspimpl.LATEST_TAG";
 
+    /**
+     * Sets the latest tag encountered.
+     *
+     * @param context The Velocity context.
+     * @param tag The tag.
+     */
     public static void setLatestTag(Context context, JspTag tag) {
         context.put(LATEST_TAG_ATTRIBUTE_NAME, tag);
     }
 
+    /**
+     * Returns the latest tag encountered.
+     *
+     * @param context The Velocity context.
+     * @return The latest tag.
+     */
     public static JspTag getLatestJspTag(Context context) {
         return (JspTag) context.get(LATEST_TAG_ATTRIBUTE_NAME);
     }
 
+    /**
+     * If necessary, wraps a {@link SimpleTag} into a {@link Tag}.
+     *
+     * @param tag The tag to (possibly) wrap.
+     * @return The wrapped tag, or the tag passed as parameter if it was not necessary.
+     */
     public static Tag wrapTag(JspTag tag) {
         if (tag == null) {
             return null;
@@ -45,6 +88,16 @@ public class JspUtils
                         + tag.getClass().getCanonicalName());
     }
 
+    /**
+     * Executes a {@link SimpleTag}.
+     *
+     * @param context The directive context.
+     * @param node The main node of the directive.
+     * @param pageContext The page context.
+     * @param tag The tag to execute.
+     * @throws JspException If something goes wrong.
+     * @throws IOException If something goes wrong.
+     */
     public static void executeSimpleTag(InternalContextAdapter context,
             Node node, PageContext pageContext, SimpleTag tag)
             throws JspException, IOException
@@ -54,6 +107,16 @@ public class JspUtils
         tag.doTag();
     }
 
+    /**
+     * Executes a {@link Tag}.
+     *
+     * @param context The directive context.
+     * @param node The main node of the directive.
+     * @param pageContext The page context.
+     * @param tag The tag to execute.
+     * @throws JspException If something goes wrong.
+     * @throws IOException If something goes wrong.
+     */
     public static void executeTag(InternalContextAdapter context, Node node,
             PageContext pageContext, Tag tag) throws JspException
     {
--- velocity-tools-view-jsp/src/main/java/org/apache/velocity/tools/view/jsp/jspimpl/VelocityBodyContent.java
+++ velocity-tools-view-jsp/src/main/java/org/apache/velocity/tools/view/jsp/jspimpl/VelocityBodyContent.java
@@ -1,4 +1,23 @@
-package org.apache.velocity.tools.view.jsp.taglib.jspimpl;
+package org.apache.velocity.tools.view.jsp.jspimpl;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 import java.io.IOException;
 import java.io.Reader;
@@ -15,13 +34,30 @@ import org.apache.velocity.exception.ParseErrorException;
 import org.apache.velocity.exception.ResourceNotFoundException;
 import org.apache.velocity.runtime.parser.node.ASTBlock;
 
+/**
+ * Publishes a block inside a Velocity directive as a {@link BodyContent}.
+ *
+ */
 public class VelocityBodyContent extends BodyContent
 {
 
+    /**
+     * The block to expose.
+     */
     private ASTBlock block;
 
+    /**
+     * The directive context.
+     */
     private InternalContextAdapter context;
 
+    /**
+     * Constructor.
+     *
+     * @param jspWriter The JSP writer to be used by default.
+     * @param block The block to wrap.
+     * @param context The directive context.
+     */
     public VelocityBodyContent(JspWriter jspWriter, ASTBlock block,
             InternalContextAdapter context)
     {
@@ -47,16 +83,20 @@ public class VelocityBodyContent extends BodyContent
             return stringWriter.toString();
         } catch (MethodInvocationException e)
         {
-            throw new VelocityToolsJspException("Cannot invoke a method while rendering a body", e);
+            throw new VelocityToolsJspException(
+                    "Cannot invoke a method while rendering a body", e);
         } catch (ResourceNotFoundException e)
         {
-            throw new VelocityToolsJspException("Cannot find a resource while rendering a body", e);
+            throw new VelocityToolsJspException(
+                    "Cannot find a resource while rendering a body", e);
         } catch (ParseErrorException e)
         {
-            throw new VelocityToolsJspException("Cannot parse while rendering a body", e);
+            throw new VelocityToolsJspException(
+                    "Cannot parse while rendering a body", e);
         } catch (IOException e)
         {
-            throw new VelocityToolsJspException("I/O exception while rendering a body", e);
+            throw new VelocityToolsJspException(
+                    "I/O exception while rendering a body", e);
         }
     }
 
