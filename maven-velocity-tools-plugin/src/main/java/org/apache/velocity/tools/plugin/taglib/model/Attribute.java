411f2046e0a507627a777afc3d8c59c140476ac9
Antonio Petrelli <apetrelli@apache.org>
2010-12-17 14:31:10 +0000

    VELTOOLS-132 Added Javadocs.
    git-svn-id: https://svn.apache.org/repos/asf/velocity/tools/trunk@1050400 13f79535-47bb-0310-9956-ffa450edef68

--- maven-velocity-tools-plugin/src/main/java/org/apache/velocity/tools/plugin/taglib/model/Attribute.java
+++ maven-velocity-tools-plugin/src/main/java/org/apache/velocity/tools/plugin/taglib/model/Attribute.java
@@ -1,5 +1,24 @@
 package org.apache.velocity.tools.plugin.taglib.model;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,10 +26,16 @@ import org.apache.commons.digester.annotations.rules.BeanPropertySetter;
 import org.apache.commons.digester.annotations.rules.ObjectCreate;
 import org.apache.commons.digester.annotations.rules.SetTop;
 
+/**
+ * It represents a tag attribute, in a tag library descriptor.
+ */
 @ObjectCreate(pattern = "taglib/tag/attribute")
 public class Attribute
 {
 
+    /**
+     * Maps primitive types to their wrapped equivalents.
+     */
     private static Map<String, String> primitive2wrapped;
 
     static {
@@ -25,84 +50,167 @@ public class Attribute
         primitive2wrapped.put("boolean", Boolean.class.getName());
     }
 
+    /**
+     * The name of the attribute.
+     */
     @BeanPropertySetter(pattern = "taglib/tag/attribute/name")
     private String name;
 
+    /**
+     * The description of the attribute.
+     */
     @BeanPropertySetter(pattern = "taglib/tag/attribute/description")
     private String description;
 
+    /**
+     * It tells if the attribute is required.
+     */
     @BeanPropertySetter(pattern = "taglib/tag/attribute/required")
     private boolean required = false;
 
+    /**
+     * It tells if the attribute can have a value that is the result of runtime expression.
+     */
     @BeanPropertySetter(pattern = "taglib/tag/attribute/rtexprvalue")
     private boolean rtexprvalue = false;
 
+    /**
+     * The type of the attribute.
+     */
     @BeanPropertySetter(pattern = "taglib/tag/attribute/type")
     private String type;
 
+    /**
+     * The containing tag.
+     */
     private Tag tag;
 
+    /**
+     * Returns the name of the attribute.
+     *
+     * @return The name of the attribute.
+     */
     public String getName()
     {
         return name;
     }
 
+    /**
+     * Sets the name of the attribute.
+     *
+     * @param name The name of the attribute.
+     */
     public void setName(String name)
     {
         this.name = name;
     }
 
+    /**
+     * Returns the description of the attribute.
+     *
+     * @return The description of the attribute.
+     */
     public String getDescription()
     {
         return description;
     }
 
+    /**
+     * Sets the description of the attribute.
+     *
+     * @param description The description of the attribute.
+     */
     public void setDescription(String description)
     {
         this.description = description;
     }
 
+    /**
+     * Returns <code>true</code> if the attribute is required.
+     *
+     * @return <code>true</code> if the attribute is required.
+     */
     public boolean isRequired()
     {
         return required;
     }
 
+    /**
+     * It tells if the attribute is required.
+     *
+     * @param required <code>true</code> if the attribute is required.
+     */
     public void setRequired(boolean required)
     {
         this.required = required;
     }
 
+    /**
+     * Returns <code>true</code> if the attribute accepts runtime expression.
+     *
+     * @return <code>true</code> if the attribute accepts runtime expression.
+     */
     public boolean isRtexprvalue()
     {
         return rtexprvalue;
     }
 
+    /**
+     * It tells ifthe attribute accepts runtime expression.
+     *
+     * @param rtexprvalue <code>true</code> if the attribute accepts runtime expression.
+     */
     public void setRtexprvalue(boolean rtexprvalue)
     {
         this.rtexprvalue = rtexprvalue;
     }
 
+    /**
+     * Returns the type of the attribute.
+     *
+     * @return The type of the attribute.
+     */
     public String getType()
     {
         return type;
     }
 
+    /**
+     * Sets the type of the attribute.
+     *
+     * @param type The type of the attribute.
+     */
     public void setType(String type)
     {
         this.type = type;
     }
 
+    /**
+     * Sets the tag that contains this attribute.
+     *
+     * @param tag The container tag.
+     */
     @SetTop(pattern="taglib/tag/attribute")
-    public void setTag(Tag tag)
+    public void setJTag(Tag tag)
     {
         this.tag = tag;
     }
 
+    /**
+     * Returns the tag that contains this attribute.
+     *
+     * @return The container tag.
+     */
     public Tag getTag()
     {
         return tag;
     }
 
+    /**
+     * Returns the type that should be used in the Velocity directive for this attribute.
+     *
+     * @return The type to use in Velocity directive.
+     */
     public String getWrappedType() {
         if (type == null) {
             return String.class.getName();
@@ -114,6 +222,11 @@ public class Attribute
         return retValue;
     }
 
+    /**
+     * Returns the name of the setter method.
+     *
+     * @return The name of the setter method.
+     */
     public String getSetterName() {
         return "set" + name.substring(0, 1).toUpperCase() + name.substring(1);
     }
--- maven-velocity-tools-plugin/src/main/java/org/apache/velocity/tools/plugin/taglib/model/Taglib.java
+++ maven-velocity-tools-plugin/src/main/java/org/apache/velocity/tools/plugin/taglib/model/Taglib.java
@@ -1,5 +1,24 @@
 package org.apache.velocity.tools.plugin.taglib.model;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,68 +26,136 @@ import org.apache.commons.digester.annotations.rules.BeanPropertySetter;
 import org.apache.commons.digester.annotations.rules.ObjectCreate;
 import org.apache.commons.digester.annotations.rules.SetNext;
 
+/**
+ * It represents a Tag Library, as read from a Tag Library Descriptor.
+ */
 @ObjectCreate(pattern = "taglib")
 public class Taglib
 {
+    /**
+     * The description of the taglib.
+     */
     @BeanPropertySetter(pattern = "taglib/description")
     private String description;
 
+    /**
+     * The version of this taglib.
+     */
     @BeanPropertySetter(pattern = "taglib/tlib-version")
     private String tlibVersion;
 
+    /**
+     * The short name of the taglib.
+     */
     @BeanPropertySetter(pattern = "taglib/short-name")
     private String shortName;
 
+    /**
+     * The normalized URI to access this taglib.
+     */
     @BeanPropertySetter(pattern = "taglib/uri")
     private String uri;
 
+    /**
+     * The contained tags.
+     */
     private List<Tag> tags = new ArrayList<Tag>();
 
+    /**
+     * Returns the description.
+     *
+     * @return The description.
+     */
     public String getDescription()
     {
         return description;
     }
 
+    /**
+     * Sets the description.
+     *
+     * @param description The description.
+     */
     public void setDescription(String description)
     {
         this.description = description;
     }
 
+    /**
+     * Returns the taglib version.
+     *
+     * @return The taglib version.
+     */
     public String getTlibVersion()
     {
         return tlibVersion;
     }
 
+    /**
+     * Sets the taglib version.
+     *
+     * @param tlibVersion The taglib version.
+     */
     public void setTlibVersion(String tlibVersion)
     {
         this.tlibVersion = tlibVersion;
     }
 
+    /**
+     * Returns the short name of the taglib.
+     *
+     * @return The short name of the taglib.
+     */
     public String getShortName()
     {
         return shortName;
     }
 
+    /**
+     * Sets the short name of the taglib.
+     *
+     * @param shortName The short name of the taglib.
+     */
     public void setShortName(String shortName)
     {
         this.shortName = shortName;
     }
 
+    /**
+     * Returns the URI of the taglib.
+     *
+     * @return The URI of the taglib.
+     */
     public String getUri()
     {
         return uri;
     }
 
+    /**
+     * Sets the URI of the taglib.
+     *
+     * @param uri The URI of the taglib.
+     */
     public void setUri(String uri)
     {
         this.uri = uri;
     }
 
+    /**
+     * Returns the tags of this taglib.
+     *
+     * @return The tags.
+     */
     public List<Tag> getTags()
     {
         return tags;
     }
 
+    /**
+     * Adds a new tag to the contained tag.
+     *
+     * @param tag The tag to add.
+     */
     @SetNext
     public void addTag(Tag tag)
     {
