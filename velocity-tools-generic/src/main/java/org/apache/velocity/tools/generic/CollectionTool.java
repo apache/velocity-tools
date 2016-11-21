6e7bbef44e3d6af7ffed04180739103e50728479
Sergiu Dumitriu <sdumitriu@apache.org>
2016-11-21 15:50:47 +0000

    [cleanup] Removed unused imports
    git-svn-id: https://svn.apache.org/repos/asf/velocity/tools/trunk@1770710 13f79535-47bb-0310-9956-ffa450edef68

--- velocity-tools-generic/src/main/java/org/apache/velocity/tools/generic/CollectionTool.java
+++ velocity-tools-generic/src/main/java/org/apache/velocity/tools/generic/CollectionTool.java
@@ -22,13 +22,11 @@ package org.apache.velocity.tools.generic;
 import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.velocity.tools.config.DefaultKey;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
